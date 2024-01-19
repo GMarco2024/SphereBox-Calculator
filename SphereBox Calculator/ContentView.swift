41a08ea305cfc6c17e5cad9c89e55396a9e88fc5
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-01-19 16:55:01 -0600

    Initial Commit

---
+++ SphereBox Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SphereBox Calculator
+//
+//  Created by PHYS 440 Marco on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SphereBox Calculator/SphereBox_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereBox_CalculatorApp.swift
+//  SphereBox Calculator
+//
+//  Created by PHYS 440 Marco on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SphereBox_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
